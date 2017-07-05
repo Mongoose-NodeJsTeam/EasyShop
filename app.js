8dc482f010e122c2ef7849fcf4fa8792b562123b
angel75013 <angelyazarov@yahoo.com>
2017-07-05 16:33:07 +0300

    app updated

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const appConstants = require('./app/common/app-constants');
 
 const app = express();
 
+const data = require('./app/data');
+
 require('./app/config/app-config')(app);
+require('./app/config/auth-config')(app, data);
 require('./app/routers')(app);
 
 app.listen(appConstants.APP_PORT, () => {
