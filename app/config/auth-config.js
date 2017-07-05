324fdc3d098209b67ac5771d3081463351c85fe1
angel75013 <angelyazarov@yahoo.com>
2017-07-05 16:30:38 +0300

    passport configuration added

---
+++ app/config/auth-config.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const Strategy = require('passport-local');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const configAuth = (app, data) => {
+    app.use(cookieParser());
+    app.use(session({ secret: 'Purple Unicorn' }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new Strategy(
+        (username, password, done) => {
+            data.users.findByUsername(username)
+                .then((user) => {
+                    return done(null, user);
+                })
+                .catch((err) => {
+                    return done(err);
+                });
+
+            passport.serializeUser((user, done) => {
+                done(null, user.id);
+            });
+
+            passport.deserializeUser(function(id, done) {
+                return data.users.findUserById(id)
+                    .then((user) => {
+                        done(null, user);
+                    })
+                    .catch(done);
+            });
+        }
+    ));
+};
+
+module.exports = configAuth;
