add241b86b3f8a8d2bf84f18534eba590e51d8da
angel75013 <angelyazarov@yahoo.com>
2017-07-05 16:32:38 +0300

    app-configue updated

--- app/config/app-config.js
+++ app/config/app-config.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const configApp = (app) => {
     app.set('view engine', 'pug');
     app.set('views', path.join(__dirname, '../views'));
+	
+	app.use(bodyParser.urlencoded({ extended: false }));
 
     app.use('/libs', express.static(path.join(__dirname, '../../node_modules')));
 
