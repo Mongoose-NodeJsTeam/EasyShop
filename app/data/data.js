b42e115bdaffbf1aba4715ab8bb4d689c9f50b67
angel75013 <angelyazarov@yahoo.com>
2017-07-05 16:32:08 +0300

    data folder added

---
+++ app/data/data.js
@@ -0,0 +1,41 @@
+const usersList = [{
+    id: 1,
+    username: 'jhon',
+    password: '123456'
+}
+
+];
+
+const users = {
+    findByUsername(username) {
+        const usernameToLower = username.toLowerCase();
+        const user =
+            usersList.find((u) =>
+                u.username.toLowerCase() === usernameToLower);
+            return new Promise((resolve, reject) => {
+                if (!user) {
+                    return reject('No user found!');
+                } else {
+                    return resolve(user);
+                }
+            });
+    },
+    findUserById(id) {
+        id = parseInt(id, 10);
+
+        const user =
+            usersList.find((u) =>
+            u.id === id);
+        return new Promise((resolve, reject) => {
+            if (!user) {
+                return reject('No user found!');
+            } else {
+                return resolve(user);
+            }
+        });
+    },
+};
+
+module.exports = {
+    users
+};
