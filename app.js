3e85f251b829b660fd6dde58fddedd724ad5e9f2
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-05 10:39:41 +0300

    App-config extended!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const app = require('./app/config/app-config');
+const express = require('express');
 const appConstants = require('./app/common/app-constants');
 
+const app = express();
+
+require('./app/config/app-config')(app);
 require('./app/routers')(app);
 
 app.listen(appConstants.APP_PORT, () => {
