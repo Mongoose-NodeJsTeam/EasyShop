77898f4f43ee2fdc5b6a1c5ac74f1722a8f36a1c
angel75013 <angelyazarov@yahoo.com>
2017-07-09 11:02:07 +0300

    nav user profile added

---
+++ app/controllers/profile-controller.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    return {
+        loadProfilePage(req, res) {
+            console.log(req.user)
+
+            res.status(200)
+                .render('profile', {user: req.user});
+        }
+    };
+};
