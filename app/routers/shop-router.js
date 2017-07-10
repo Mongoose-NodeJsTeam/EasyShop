754f2be0aa6efaebd514c6b917d5baaa900462d1
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-10 22:02:10 +0300

    Shop-router added.

---
+++ app/routers/shop-router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+module.exports = function (app) {
+    const shopController = require('../controllers/shop-controller')();
+
+    const shopRouter = new express.Router();
+
+    shopRouter
+        .get('/my-shops', shopController.loadMyShopsPage)
+        .get('/add-shop', shopController.loadAddNewShopPage)
+        .post('/add-shop', shopController.createNewShop);
+
+    app.use('/shop', shopRouter);
+
+};
