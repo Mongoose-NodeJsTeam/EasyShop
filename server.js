308d4f368d0f657fe73643f0effdd80f0384cca8
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-26 13:45:17 +0300

    New starting file added 'server.js'.

---
+++ server.js
@@ -0,0 +1,17 @@
+const async = () => {
+    return Promise.resolve();
+};
+
+const constants = require('./app/utils/app-constants');
+
+async()
+    .then(() => require('./db').init(constants.connectionString))
+    .then((db) => require('./data').init(db))
+    .then((data) => require('./app').init(data))
+    .then((app) => {
+        app.listen(constants.appPort, () =>
+            console.log(`Server running at : ${constants.appPort}`));
+    })
+    .catch((err) => {
+        console.log(err);
+    });
