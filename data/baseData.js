2f6737dc01e5f28166cd537f643f2ee8b39d8090
angel75013 <angelyazarov@yahoo.com>
2017-07-26 14:22:32 +0300

    base data abstraction created

---
+++ data/baseData.js
@@ -0,0 +1,57 @@
+const ObjectID = require('mongodb').ObjectID;
+
+class BaseData {
+    constructor(db, ModelClass) {
+        this.db = db;
+        this.ModelClass = ModelClass;
+        this.collectionName = this._getCollectionName();
+        this.collection = this.db.collection(this.collectionName);
+    }
+
+    getAll() {
+        return this.collection.find().toArray();
+    }
+
+    findById(id) {
+        return this.collection.findOne({
+            _id: new ObjectID(id),
+        });
+    }
+
+    filterBy(props) {
+        return this.collection.find(props).toArray();
+    }
+
+    create(model) {
+        if (!this._isModelValid(model)) {
+            return Promise.reject('Model validation failed!');
+        }
+        return this.collection.insertOne(model)
+            .then(() => {
+                return this.ModelClass.toViewModel(model);
+            });
+    }
+
+    updateById(model) {
+        return this.collection.updateOne({
+            _id: model._id,
+        }, model);
+    }
+
+    deleteById(id) {
+        return this.collection.deleteOne({
+            '_id': new ObjectID(id)
+        });
+    }
+
+    _isModelValid(model) {
+        return this.ModelClass.validator(model);
+    }
+
+    _getCollectionName() {
+        return this.ModelClass.name.toLowerCase() + 's';
+    }
+}
+
+
+module.exports = BaseData;
