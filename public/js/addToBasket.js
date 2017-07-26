bdcb2925f09794c29b8ba99259e89284b80eacfb
angel75013 <angelyazarov@yahoo.com>
2017-07-26 20:44:32 +0300

    update+fix addToBasket

--- public/js/addToBasket.js
+++ public/js/addToBasket.js
@@ -1,9 +1,9 @@
 /* globals $*/
 $(document).ready(function () {
-    $('.quantitySetter').on('change',function(){
+    $('.quantitySetter').on('change', function () {
 
-        const quantity=$(this).val();
-        $(this).next().attr('data-quantity',quantity);
+        const quantity = $(this).val();
+        $(this).next().attr('data-quantity', quantity);
 
 
     });
@@ -21,22 +21,39 @@ $(document).ready(function () {
         const productName = $(e.target).attr('data-name');
         const productPrice = $(e.target).attr('data-price');
         let productQuantity = $(e.target).attr('data-quantity');
-        if(productQuantity===undefined){productQuantity=0}
-        const prodTotal=(+productPrice*+productQuantity).toFixed(2);
+        if (productQuantity === undefined) {
+            productQuantity = 0
+        }
+        const prodTotal = (+productPrice * +productQuantity).toFixed(2);
 
 
         const product = {
+            key: counter,
             id: productId,
             shopId: shopId,
             name: productName,
             price: productPrice,
-           quantity:productQuantity.toString(),
+            quantity: productQuantity.toString(),
             total: prodTotal
 
         };
+        //alert ako sa razlichni shops
+        if (counter > 1) {
+            let keysLocalStorage=Object.keys(localStorage);
+            for (let key of keysLocalStorage) {
+                if (Number(key)&&Number(key)!==counter) {
+                    let productAlreadyInShop = JSON.parse(localStorage[key]);
+                    //proverka za dali e sashtiq user
+                    if (productAlreadyInShop.shopId !== product.shopId) {
+                        alert("The products must be from the same shop");
+                        window.location = '/shop/' + productAlreadyInShop.shopId;
+                    }
+                }
+            }
+        }
 
 
-        localStorage[counter] =JSON.stringify(product);
+        localStorage[counter] = JSON.stringify(product);
 
 
     });
