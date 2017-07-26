13bb15879824baee22662291fccd68b0299f062e
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-26 13:54:37 +0300

    (Refactoring structure): Data folder outside app. and usersData improved.

---
+++ data/usersData.js
@@ -0,0 +1,53 @@
+const BaseData = require('./baseData');
+const User = require('../models/userModel');
+const ObjectID = require('mongodb').ObjectID;
+
+class UsersData extends BaseData {
+    constructor(db) {
+        super(db, User);
+    }
+
+    findByUsername(username) {
+        return this.filterBy({
+            username: new RegExp(username, 'i')
+        })
+            .then(([user]) => user);
+    }
+
+    checkPassword(username, password) {
+        return this.findByUsername(username)
+            .then((user) => {
+                if (!user) {
+                    throw new Error('Invalid user');
+                }
+
+                if (user.password !== password) {
+                    throw new Error('Invalid password');
+                }
+
+                return true;
+            });
+    }
+
+    assignTripshoptoUser(user, tripshop) {
+        if (user.tripshops) {
+            return this.collection.updateOne({
+                _id: new ObjectID(user._id)
+            }, {
+                $push: {
+                    tripshops: tripshop
+                }
+            });
+        } else {
+            return this.collection.updateOne({
+                    _id: new ObjectID(user._id)
+                }, {
+                    $set: {
+                        tripshops: [tripshop]
+                    }
+                });
+        }
+    }
+}
+
+module.exports = UsersData;
