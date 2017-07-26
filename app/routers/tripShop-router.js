b919870454d99f5a2e46cefcf2b1342a2b3f37b8
angel75013 <angelyazarov@yahoo.com>
2017-07-26 14:19:22 +0300

    tripshop routed created

---
+++ app/routers/tripShop-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const attachTo = (app, data) => {
+    const tripshopRouter = new express.Router({ mergeParams: true });
+
+    const tripshopController = require('../controllers/tripShop-controller')
+        .init(data);
+
+    tripshopRouter
+        .get('/', tripshopController.loadTripshopForm
+            .bind(tripshopController))
+        .get('/findTripshop', tripshopController.loadAllTripshops
+            .bind(tripshopController))
+        .post('/add-tripshop', tripshopController.createTripshop
+            .bind(tripshopController))
+        .get('/add-tripshop', tripshopController.loadTripshopCreationalForm
+            .bind(tripshopController));
+
+    app.use('/tripshops', tripshopRouter);
+};
+
+module.exports = { attachTo };
