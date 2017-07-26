0655342eecdeeb0070efce0aff15ab35c01ecf63
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-26 13:47:32 +0300

    HomeController improved.

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,17 +1,22 @@
-module.exports = function() {
-    return {
-        loadHomePage(req, res) {
-            return Promise.resolve()
-                .then(() => {
-                    if (!req.isAuthenticated()) {
-                        res.render('homePage', {});
-                    } else {
-                        res.status(200)
-                            .render('homePage', {
-                                user: req.user
-                            });
-                    }
+class HomeController {
+    constructor(data) {
+        this.data = data;
+    }
+
+    loadHomePage(req, res) {
+        if (!req.isAuthenticated()) {
+            res.render('homePage', {});
+        } else {
+            res.status(200)
+                .render('homePage', {
+                    user: req.user
                 });
         }
-    };
+    }
+}
+
+const init = (data) => {
+    return new HomeController(data);
 };
+
+module.exports = { init, };
