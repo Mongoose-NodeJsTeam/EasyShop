4775cb8a9241e5279d179cd17cb15e18f158387e
angel75013 <angelyazarov@yahoo.com>
2017-07-26 14:16:05 +0300

    shop controller changed and add new functions

--- app/controllers/shop-controller.js
+++ app/controllers/shop-controller.js
@@ -1,44 +1,134 @@
-const proUserData = require('../data/proUserData');
+class ShopController {
+    constructor(data) {
+        this.data = data;
+    }
 
-module.exports = function () {
-    return {
-        loadMyShopsPage(req, res) {
-            res.status(200)
-                .render('shop/myShopsPage', {
-                    user: req.user
+    getAddShopForm(req, res) {
+        return res.render('shop/addNewShop');
+    }
+
+    createShop(req, res) {
+        const shop = req.body;
+        const user = req.user;
+
+        this.data.shops.findByShopName(shop.name)
+            .then((foundShop) => {
+                if (foundShop) {
+                    return res.redirect('/shop/add-shop');
+                } else {
+                    shop.user = {
+                        id: user._id,
+                        username: user.username,
+                        isProUser: user.isProUser
+                    };
+
+                    return Promise.all(
+                        [
+                            this.data.shops.create(shop),
+                            this.data.users.findByUsername(user.username),
+                        ]
+                    ).then(([dbShop, dbUser]) => {
+                        dbUser.username = user.username;
+                        dbUser.password = user.password;
+                        dbUser.address = user.address;
+                        dbUser.isProUser = user.isProUser;
+                        dbUser.email = user.email;
+                        dbUser.tripshops = user.tripShops || [];
+
+                        dbUser.shops = user.shops || [];
+                        dbUser.shops.push({
+                            name: dbShop.name,
+                            user: dbShop.username,
+                            address: dbShop.address,
+                            mobile: dbShop.mobile,
+                            email: dbShop.email,
+                            description: dbShop.description,
+                            products: dbShop.products || []
+                        });
+
+                        return this.data.users.updateById(dbUser);
+                    })
+                        .then(() => {
+                            req.flash(
+                                'success',
+                                'New shop added successfully!!');
+                            return res.redirect('/shop/my-shops');
+                        })
+                        .catch((err) => {
+                            console.log('shiiiiiiit');
+                            req.flash('error', err);
+                            return res.redirect('/shop/add-shop');
+                        });
+                }
+            });
+    }
+
+    loadMyShopsPage(req, res) {
+        const userId = req.user._id;
+
+        this.data.shops.findAllUserShopsByUserId(userId)
+            .then((shops) => {
+                res.render('shop/myShopsPage', {
+                    user: req.user,
+                    shops: shops
                 });
-        },
-        loadAddNewShopPage(req, res) {
-            res.status(200)
-                .render('shop/addNewShop', {
-                    user: req.user
+            });
+    }
+
+    loadSpecifiedShop(req, res) {
+        const shopId = req.params.id;
+
+        this.data.shops.findById(shopId)
+            .then((shop) => {
+                return res.render('shop/shopDetails', {
+                    user: req.user,
+                    shop: shop
                 });
-        },
-        createNewShop(req, res) {
-            const modelShop = req.body;
+            });
+    }
 
-            proUserData.proUsers.addNewShop(
-                req.user.id,
-                modelShop.name,
-                modelShop.address,
-                modelShop.email,
-                modelShop.mobile,
-                modelShop.description
-            );
+    loadGeneralInfoAboutShop(req, res) {
+        const shopId = req.params.id;
 
-            res.redirect('my-shops');
-        },
-        loadSpecifiedShop(req, res) {
-            const userId = req.user.id;
-            const shopId = req.params.id;
-
-            return proUserData.proUsers.findShopById(userId, shopId)
-                .then((shop) => {
-                    res.render('shop/shopDetails', {
-                        user: req.user,
-                        shop: shop
-                    });
+        this.data.shops.findById(shopId)
+            .then((shop) => {
+                return res.send({
+                    shop: shop
                 });
-        }
-    };
+            });
+    }
+
+    updateSpecifiedShop(req, res) {
+        const shopEditData = req.body;
+        const shopId = req.params.id;
+
+        return this.data.shops.updateShopById(shopId, shopEditData)
+            .then(() => {
+                req.flash(
+                    'success',
+                    'Shop was successfully updated');
+                // return res.redirect('/shop/my-shops');
+                return res.send({ locationUrl: '/shop/my-shops' });
+            })
+            .catch((err) => {
+                req.flash('error', err);
+                return res.send({ locationUrl: '/shop/my-shops' });
+            });
+    }
+
+    deleteShop(req, res) {
+        const shopId = req.params.id;
+
+        this.data.shops.deleteById(shopId)
+            .then(() => {
+                return res.redirect('/shop/my-shops');
+            });
+    }
+}
+
+const init = (data) => {
+    return new ShopController(data);
 };
+
+module.exports = {init};
+
