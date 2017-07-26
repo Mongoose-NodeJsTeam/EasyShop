ed1f833fd3da043951770d1a7dbfdddb918ce51a
angel75013 <angelyazarov@yahoo.com>
2017-07-26 14:51:13 +0300

    displaing actual basket

---
+++ public/js/loadBasketItems.js
@@ -0,0 +1,37 @@
+/* globals $*/
+$(document).ready(function () {
+
+    $('#loadShoppingCart').on('click', function () {
+
+//1.strigify objecta kogato se sazdava, vkarvash go v storaga
+// 2. prashta se vinagi stringifynat object
+// 3. vseki edin input field predstavlqva property v objecta teq.body
+// .4 ako e masiv ot obekti pristiga kato req.body . name :[object object],koeto v deistvitelnost predstavqva stringifinatiqt obekt
+// 5 pravi se iteraciq po propertita na req.body i se parstva vsqko valu
+
+
+        var form = $("<form/>",
+            {
+                action: '/basket',
+                method: 'POST',
+                headers: {
+                    "content-type": "application/json"
+                }
+            });
+        for (let i = 1; i < localStorage.length; i++) {
+
+            $('<input>').attr('type', 'hidden')
+                .attr('name', "product" + i)
+                .attr('value', (localStorage.getItem(i.toString())))
+                .appendTo(form)
+
+        }
+
+        $(document.body).append(form);
+        form.submit();
+
+
+        return false; // cancel the actual link
+    });
+
+});
