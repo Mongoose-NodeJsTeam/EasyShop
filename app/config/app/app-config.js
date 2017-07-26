7f93a5edc91dcc486966e2bc7c8d86f14e24f95a
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-26 13:38:56 +0300

    (Refactoring structure): App-config improved. Flash messages, cookie parser moved to auth-config.

---
+++ app/config/app/app-config.js
@@ -0,0 +1,29 @@
+/* globals __dirname */
+
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const applyTo = (app) => {
+    app.set('view engine', 'pug');
+    app.set('views', path.join(__dirname, '../../views'));
+
+    app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+
+    const libsPath = path.join(__dirname, '../../../node_modules');
+    app.use('/libs', express.static(libsPath));
+
+    const publicPath = path.join(__dirname, '../../../public');
+    app.use('/public', express.static(publicPath));
+
+    app.use((req, res, next) => {
+        res.locals.messages = require('express-messages')(req, res);
+        next();
+    });
+    app.use(require('connect-flash')());
+
+};
+
+module.exports = { applyTo };
