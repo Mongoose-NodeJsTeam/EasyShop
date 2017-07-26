08d81a528523906190d21d2907af69e4cb829c4e
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-26 13:46:44 +0300

    ShopModel and userModel added and updated.

---
+++ models/shopModel.js
@@ -0,0 +1,36 @@
+const ModelsValidator = require('../app/utils/models-validator');
+
+class Shop {
+    static validator(model) {
+        if (ModelsValidator._validateString(
+                model.name, 1, 20, /[a-zA-Z0-9_\\.]/) &&
+            ModelsValidator._validateString(
+                model.address, 10, 60, /[a-zA-Z0-9_\\.]/) &&
+            ModelsValidator._validateEmail(
+                model.email) &&
+            ModelsValidator._validateMobile(
+                model.mobile) &&
+            ModelsValidator._validateString(
+                model.description, 10, 40, /[a-zA-Z0-9_\\.]/)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    static toViewModel(model) {
+        const viewModel = new Shop();
+
+        Object.keys(model)
+            .forEach((prop) => {
+                viewModel[prop] = model[prop];
+            });
+        return viewModel;
+    }
+}
+
+module.exports = Shop;
---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const ModelsValidator = require('../app/utils/models-validator');
+
+class User {
+    static validator(model) {
+        if (ModelsValidator._validateString(
+                model.username, 2, 15, /[a-zA-Z0-9_\\.]/) &&
+            ModelsValidator._validateEmail(
+                model.email) &&
+            ModelsValidator._validateString(
+                model.address, 10, 60, /[a-zA-Z0-9_\\.]/) &&
+            ModelsValidator._validatePassword(
+                model.password)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    static toViewModel(model) {
+        const viewModel = new User();
+
+        Object.keys(model)
+            .forEach((prop) => {
+                viewModel[prop] = model[prop];
+            });
+        return viewModel;
+    }
+}
+
+module.exports = User;
