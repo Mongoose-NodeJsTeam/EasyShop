434c87a11ac48a947265a8f34422a9265c319337
angel75013 <angelyazarov@yahoo.com>
2017-07-26 14:16:41 +0300

    add basket controller

---
+++ app/controllers/basket-controller.js
@@ -0,0 +1,41 @@
+class BasketController {
+    constructor(data) {
+        this.data = data;
+    }
+
+    loadBasketPage(req, res) {
+        const user = req.user;
+        const products = (req.body);
+
+        const arr=[];
+
+        for (const key in products) {
+            if (products.hasOwnProperty(key)) {
+                const item =JSON.parse(products[key]);
+
+                const prod=(item);
+                arr.push((prod));
+            }
+        }
+
+
+        res.render('basket', {
+            products: arr
+        });
+    }
+
+    addProductToBasket(req, res) {
+        console.log(req.body.product + 'aideee');
+        console.log(req.body.name + 'aideeddedee');
+        res.send('successuflly added');
+
+        //initialize basket property user,update
+        // add productto basket
+    }
+}
+
+const init = (data) => {
+    return new BasketController(data);
+};
+
+module.exports = { init };
