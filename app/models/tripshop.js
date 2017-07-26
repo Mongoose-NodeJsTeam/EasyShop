c01e88616106a0fcc1825d73a86fe1ba11dfb4ef
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-26 13:44:23 +0300

    (Refactoring Structure): Delete files and folders

--- app/models/tripshop.js
+++
@@ -1,9 +0,0 @@
-class TripShop {
-    constructor(regularUser, date) {
-        this.regularUser = regularUser;
-        this.date = date;
-        this.users = [];
-    }
-}
-module.exports = TripShop;
-
