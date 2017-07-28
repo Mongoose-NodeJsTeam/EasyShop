b1edf4aa82de4976fa91ee54147ec857369638da
angel75013 <angelyazarov@yahoo.com>
2017-07-28 16:47:27 +0300

    home controller profile route added

--- app/routers/home-router.js
+++ app/routers/home-router.js
@@ -4,9 +4,9 @@ const attachTo = (app, data) => {
     const homeController = require('../controllers/home-controller').init(data);
 
     const homeRouter = new express.Router();
-
     homeRouter
-        .get('/', homeController.loadHomePage);
+        .get('/', homeController.loadHomePage)
+        .get('/profile', homeController.loadProfilePage);
 
     app.use('/', homeRouter);
 };
