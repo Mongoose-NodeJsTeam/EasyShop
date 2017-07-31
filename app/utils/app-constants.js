102eb474c94e41c218ccca1d0002ff5abc5e862d
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-07-31 10:58:06 +0300

    Authorization view changed.

--- app/utils/app-constants.js
+++ app/utils/app-constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    appPort: 3002,
+    appPort: 3003,
     connectionString: 'mongodb://mongo:mongo@ds127983.mlab.com:27983/easypeasyshop',
     sessionSecret: 'Purple Unicorn'
 };
