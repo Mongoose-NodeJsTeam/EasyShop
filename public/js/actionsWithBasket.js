864717a51edc22f7941c09d0646b744cfb5033a8
angel75013 <angelyazarov@yahoo.com>
2017-07-31 20:32:53 +0300

    js opereration with basket refactor

--- public/js/actionsWithBasket.js
+++ public/js/actionsWithBasket.js
@@ -3,23 +3,23 @@
 calculateTotal();
 
 $('.plus').click(function(e) {
-    let $target = $(e.target);
+    const $target = $(e.target);
 
     const target = $('.quantity', this.parentNode)[0];
     target.value = +target.value + 1;
     updateTotal($target);
 });
 $('.minus').click(function(e) {
-    let $target = $(e.target);
-    let target = $('.quantity', this.parentNode)[0];
+    const $target = $(e.target);
+    const target = $('.quantity', this.parentNode)[0];
     if (target.value > 1) {
         target.value = +target.value - 1;
     }
     updateTotal($target);
 });
 $('.removeProduct').click(function(e) {
-    let $target = $(e.target);
-    let parent = $target.parent().parent();
+    const $target = $(e.target);
+    const parent = $target.parent().parent();
     parent.remove();
     calculateTotal();
 });
@@ -42,7 +42,7 @@ $('#checkBasket').on('click', function() {
         const shopperId = shopperdata[0].attributes[0].value;
         const userId = shopperdata[0].attributes[1].value;
         const tripId = shopperdata[0].attributes[2].value;
-        const productName = $productData[1].innerText.trim();//TODO ima nqkuv nov red nakraq na product name
+        const productName = $productData[1].innerText.trim();
         const productQuantity = $productData[2].children[2].value;
         const productPrice = $productData[3].innerText;
         const productTotal = $productData[4].innerText;
@@ -62,25 +62,25 @@ $('#checkBasket').on('click', function() {
 
         allProduxtsInBasket.push(product);
     }
-    const form = $("<form/>",
+    const form = $('<form/>',
         {
             action: '/basket/validate',
             method: 'POST',
             headers: {
-                "content-type": "application/json"
+                'content-type': 'application/json'
             }
         });
     for (let i = 0; i < allProduxtsInBasket.length; i++) {
         $('<input>').attr('type', 'hidden')
-            .attr('name', "product" + i)
+            .attr('name', 'product' + i)
             .attr('value', JSON.stringify(allProduxtsInBasket[i]))
             .appendTo(form);
-
     }
     const keyProducts = takeKeyFromProductsInBasket();
 
     //remove items in local storage that are already validate
-    //foreach item in local storage if key=allproduxt in basket.key, localstorage.remove(key)
+    //foreach item in local storage
+    // if key=allproduxt in basket.key, localstorage.remove(key)
     const keysLocalStorage = Object.keys(localStorage);
     for (const key of keysLocalStorage) {
         let matched = false;
@@ -105,81 +105,82 @@ $('#checkBasket').on('click', function() {
 });
 
 function updateTotal($target) {
-    let sum = 0;
+    const sum = 0;
     //Add each product price to total
 
-    let prodPrice = $target.parent().siblings(".productPrice");
-    let price = prodPrice.text();
+    const prodPrice = $target.parent().siblings('.productPrice');
+    const price = prodPrice.text();
 
-    let quantity = $target.siblings(".quantity").val();
+    const quantity = $target.siblings('.quantity').val();
     //Total for one product
     let subtotal = Number(price) * Number(quantity);
     //Round to 2 decimal places.
     subtotal = subtotal.toFixed(2);
     //Display subtotal in HTML element
-    let total = $target.parent().siblings(".calculateTotal");
+    const total = $target.parent().siblings('.calculateTotal');
     total.text(subtotal);
     calculateTotal();
-
 }
 
 function calculateTotal() {
     let sum = 0;
-    let totalsEachProduct = $('.calculateTotal');
+    const totalsEachProduct = $('.calculateTotal');
 
 
 //not good innertext vij s jquery imasheza iteraciq
     for (let i = 0; i < totalsEachProduct.length; i++) {
-        let $prod = $(totalsEachProduct[i]);
-        let subTotal = Number($prod.text());
+        const $prod = $(totalsEachProduct[i]);
+        const subTotal = Number($prod.text());
         sum = Number(sum) + Number(subTotal);
         sum.toFixed(2);
-
     }
     $('#TotalBasket').text(sum);//must set lenght to 2
 }
 
 function updateBasketIfquantityModified() {
-    let allProductsinBasket = $('.productInBasket');
-    let productKeysInBasket = [];
+    const allProductsinBasket = $('.productInBasket');
+    const productKeysInBasket = [];
 
     for (let i = 0; i < allProductsinBasket.length; i++) {
-        //iterate over table and take all rows,each row=product,take key related in local storage
-        let productKey = $(allProductsinBasket[i]).attr('data-key');
+        //iterate over table and take all rows,
+        // each row=product,take key related in local storage
+        const productKey = $(allProductsinBasket[i]).attr('data-key');
 
-        //productKeysInBasket=array of the keys, matching each product in local storage
+        //productKeysInBasket=array of the keys,
+        // matching each product in local storage
         productKeysInBasket.push(productKey);
 
         //select Dom element who cantains the atribute key
-        let $prod = $('.productInBasket').filter(function () {
+        const $prod = $('.productInBasket').filter(function() {
             return $(this).attr('data-key') === productKey;
         });
         //select quantity from the input field,which is next dom element
-        let input = $prod.next().children()[2].value;
+        const input = $prod.next().children()[2].value;
 
 
-        let productInLocal = JSON.parse(localStorage[productKey]);
+        const productInLocal = JSON.parse(localStorage[productKey]);
 
         //if product quantity is set to 0 in basket=>remove item
         if (allProductsinBasket[i].quantity === 0) {
-            localStorage.removeItem(productKey)
+            localStorage.removeItem(productKey);
         }
-        //update the product in local storage with the correct quantity and total
+        //update the product in local storage
+        // with the correct quantity and total
         else {
             productInLocal.quantity = Number(input);
             productInLocal.total = Number(productInLocal.price) * Number(input);
 
             localStorage[productKey] = JSON.stringify(productInLocal);
         }
-
     }
     //remove the prodcut drom local storage, if remove from basket
-    //TODO make function for this,almost the same in chechout basket,make one file,instead of two ???
-    let keysLocalStorage = Object.keys(localStorage);
-    for (let key of keysLocalStorage) {
+
+    const keysLocalStorage = Object.keys(localStorage);
+
+    for (const key of keysLocalStorage) {
         let matched = false;
         if (Number(key)) {
-            for (let basketKey of productKeysInBasket) {
+            for (const basketKey of productKeysInBasket) {
                 if (basketKey === key) {
                     matched = true;
                 }
